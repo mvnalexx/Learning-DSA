7925aba3b982503be820ab3632a70b4f0b867768
mvnalexx <alexandr.an.yagin@gmail.com>
2025-10-10 22:10:09 +0800

    Update LL.cpp

--- LL.cpp
+++ LL.cpp
@@ -1,49 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    int data; // the data stored in the Node
-    Node* next; // the pointer to the next Node
+struct Node { // struct используем всегда для создания Node 
+    int data; // the data stored in the Node добавляем валью
+    Node* next; // the pointer to the next Node создаем поинтер на следующий ноуд
     
-    Node(int value) { // Constructor to initialize the next Node
-        data=value;
-        next = nullptr; // Initialize pointer to null
+    Node(int value) { // Constructor to initialize the next Node создвем конструктор для того чтобы давать данные
+        data=value; // присваимваем валью дате
+        next = nullptr; // Initialize pointer to null направляем поинтер на ноль, некий тэил который всегда ноль чтобы видеть конец нашего листа
     }
-};
+}; // закрываем стракт всегда ; иначе ошибка будет
 
-Node* head = nullptr;
+Node* head = nullptr; // присваиваем хэду ноль, т.к. у нас сначала пустой лист, поэтому и начало(хэд) и конец(тэил) у нас ноль
 
-void insertAtHead(int value) {
+void insertAtHead(int value) { // создаем первую функцию чтобы добавить новый ноуд на позицию хэда
     // create new Node
-    Node* newNode = new Node(value);
+    Node* newNode = new Node(value); // создаем тот самый новый ноуд через newNode = new Node(value); z
     // make new Node connected point to the current Head
-    newNode->next = head;
+    newNode->next = head; // коннектим новый ноут к прошлому хэду, тем самым ставив новый ноуд на первое место, а хэд(ноль) двигается вправо и становится обычным ноудом, а новый ноуд становится хэдом
     //connect the head to the new Node
-    head = newNode;
+    head = newNode; // ну и теперь последний шаг присваиваем переменной хэд значение нашего нового нода и тем самым завершаем вставку нового ноуда на позицию хэда
 }
 
-void printList() {
-    Node* temp = head;// use a temporary pointer to traverse the list
-    cout <<"Linked List: ";
-    while (temp!=nullptr) {
-        cout <<temp->data << " -> ";
-        temp=temp->next; // move to the next node
+void printList() { // функция печати нашего листа
+    Node* temp = head;// use a temporary pointer to traverse the list создаем временный поинтер чтобы перевернуть лист и присваиваем ему хэд чтобы начать с начала и дойти до конца листа
+    cout <<"Linked List: "; 
+    while (temp!=nullptr) { // пока временный поинтер не дойдет до тэила, т.е. конца
+        cout <<temp->data << " -> "; // печатаем значение временного ноуда
+        temp=temp->next; // move to the next node // присваиваем нашему временному ноуду следующий ноуд чтобы при повторе печатать уже второй элемент списка и так далее
     }
-    cout << "NULL" << endl;
+    cout << "NULL" << endl; // наш цикл закончится когда дойдем до нуля поэтому надо вручную ноль печатать и теперь лист официально полностью запринтен
 }
 
-void insertAtTheEnd (int value) {
+void insertAtTheEnd (int value) { // еще одна функция, теперь у нас добавляем ноуд с конца как раз перед тэилом. Т.е. по логике мы создаем новый ноуд, проверяем условие что лист не пустой. 
     Node* newNode = new Node(value);
     if (head==nullptr) {
         head=newNode;
         return;
     }
-    
+    // потом после проверки создаем временный поинтер и назначаем ему значение хэда, ранем пока поинтер нашего поинтера не равен нулю, т.е. раним до последнего ноуда, которыц стоит до тэила. 
     Node* temp = head;
     while (temp->next!=nullptr) {
         temp=temp->next;
     }
-    temp->next = newNode;
+    temp->next = newNode; // ну доранили мы до последнего элемента, теперь просто пишем что поинтер последнего ноуда указывает на новый ноуд, а новый ноуд автоматически уже на ноль указывает что-ли? странно... надо перепрроверить это. На сегодня пока все. Вернись сюда 11го октября хотя бы не надолго.
 }
 
 void deleteNode (int value) {
